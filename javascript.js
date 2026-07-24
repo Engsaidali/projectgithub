5df829789b836349efa0f5091fa2241ee1eb89f1
Said Ali <sayidcaliyasiin@gmail.com>
2026-07-24 13:41:28 -0700

    69- Remove funcation code

--- javascript.js
+++ javascript.js
@@ -6,15 +6,3 @@ let magaca = "Said Ali";
 let da = 24;
 
 // Function
-function showMessage() {
-
-    alert("welcom hoyga waxa brashadda" + magaca + "!");
-
-    document.getElementById("title").innerHTML =
-        "JavaScript Waa Shaqaynayaa!";
-
-    document.getElementById("message").innerHTML =
-        "Magacaygu waa " + magaca + " waxaana jira " + da + " sano.";
-
-    document.body.style.backgroundColor = "lightblue";
-}
