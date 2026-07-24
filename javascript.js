dea40e33daf04745fceec8adc7b0c624f635efda
Said Ali <sayidcaliyasiin@gmail.com>
2026-07-24 12:07:49 -0700

    40- updet all code java script

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
-// Display a welcome message
-console.log("Welcome to JavaScript!");
+// Marka boggu furmo
+console.log("Website-ka wuu furmay.");
+
+// Variables
+let magaca = "Abdde";
+let da = 20;
 
 // Function
-function greet(userName) {
-    return `Hello, ${userName}!`;
+function showMessage() {
+
+    alert("Ku soo dhawoow " + magaca + "!");
+
+    document.getElementById("title").innerHTML =
+        "JavaScript Waa Shaqaynayaa!";
+
+    document.getElementById("message").innerHTML =
+        "Magacaygu waa " + magaca + " waxaana jira " + da + " sano.";
+
+    document.body.style.backgroundColor = "lightblue";
 }
