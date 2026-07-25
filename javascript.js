e1186f88b0eb6ab318fab6b56adc37c54e5d84d9
Said Ali <sayidcaliyasiin@gmail.com>
2026-07-25 00:49:42 -0700

    104-upddet error

--- javascript.js
+++ javascript.js
@@ -1,6 +1,56 @@
+// basic shopping cart functionality
+// Cart array
+let cart = [];
 
-// Variables
-let magaca = "Said Ali";
-let da = 24;
+// Add product to cart
+function addCart(productName, price) {
 
-// Function
+    let product = {git lo
+        name: productName,
+        price: price
+    };
+S
+    cart.push(product);
+
+    showCart();
+}
+
+
+// Show cart items
+function showCart() {
+
+    let cartList = document.getElementById("cartList");
+    let total = document.getElementById("total");
+
+    cartList.innerHTML = "";
+
+    let totalPrice = 0;
+
+
+    cart.forEach(function(item){
+
+        let li = document.createElement("li");
+
+        li.innerHTML = item.name + " - $" + item.price;
+
+        cartList.appendChild(li);
+
+
+        totalPrice += item.price;
+
+    });
+
+
+    total.innerHTML = totalPrice;
+
+}
+
+
+// Remove all products
+function clearCart(){
+
+    cart = [];
+
+    showCart();
+
+}
